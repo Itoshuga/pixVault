74f81dfa3b9670744ddcfceff8714ee82315eb47
Itoshuga <38258159+Itoshuga@users.noreply.github.com>
2023-07-11 17:00:54 +0200

    feat: add tag section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,41 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './HomePage.css';
 
-import HeroBanner from '../../components/HeroBanner/HeroBanner';
-
+import logo from '../../assets/react.svg';
 
 const HomePage = () => {
   return (
       <>
-        <HeroBanner/>
+        <section id="hero">
+            <h4>Galerie d'Image</h4>
+            <h2>Publication et</h2>
+            <h1>Partage d'Image</h1>
+            <p>PixVault vous permet de publier des images dans des collections.</p>
+            <Link to="/">Publier</Link>
+        </section>
+
+        <section id="tag" className="section-p1">
+          <div className="tag-box">
+            <img src={logo} alt="" />
+            <h6>Cosplay</h6>
+          </div>
+          <div className="tag-box">
+            <img src={logo} alt="" />
+            <h6>Anime</h6>
+          </div>   
+          <div className="tag-box">
+            <img src={logo} alt="" />
+            <h6>Game</h6>
+          </div>   
+          <div className="tag-box">
+            <img src={logo} alt="" />
+            <h6>Artist</h6>
+          </div>   
+          <div className="tag-box">
+            <img src={logo} alt="" />
+            <h6>Other</h6>
+          </div>   
+        </section>
       </>
   );
 };
