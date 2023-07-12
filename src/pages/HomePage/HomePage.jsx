5a028450e971717a2ebde6d7049e62e48d98a0a7
Itoshuga <38258159+Itoshuga@users.noreply.github.com>
2023-07-12 14:49:22 +0200

    style: improve style for high resolution

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './HomePage.css';
 
 import TagBox from '../../components/TagBox/TagBox';
+import CollectionContainer from '../../components/CollectionContainer/CollectionContainer';
 
 import logo from '../../assets/react.svg';
 
@@ -17,13 +18,33 @@ const HomePage = () => {
             <Link to="/">Publier</Link>
         </section>
 
-        <section id="tag" className="section-p1">
-          <TagBox image={logo} title="Cosplay"/>
-          <TagBox image={logo} title="Cosplay"/>
-          <TagBox image={logo} title="Cosplay"/>
-          <TagBox image={logo} title="Cosplay"/>
-          <TagBox image={logo} title="Cosplay"/>
-          <TagBox image={logo} title="Cosplay"/>
+        <section id="gallery" className="section-p1">
+          <h2>Galerie</h2>
+          <p>Les dernières collections d'image mise en ligne</p>
+          <section id="tag">
+            <TagBox 
+              title="Anime"/>
+            <TagBox 
+              title="Artist CG"/>
+            <TagBox 
+              title="Game CG"/>
+            <TagBox 
+              title="Manga"/>
+            <TagBox 
+              title="Image Set"/>
+            <TagBox 
+              title="Autre"/>
+          </section>
+          <div className="collection-container">
+            <CollectionContainer image="https://cdn.donmai.us/sample/c8/f3/__large_spotted_genet_kemono_friends_and_1_more_drawn_by_aburaeoyaji__sample-c8f3de5c197618eac2b680a30fee1914.jpg" artistName="neocoill" collectionName="Sweet Dream"/>
+            <CollectionContainer image="https://cdn.donmai.us/sample/c8/f3/__large_spotted_genet_kemono_friends_and_1_more_drawn_by_aburaeoyaji__sample-c8f3de5c197618eac2b680a30fee1914.jpg" artistName="neocoill" collectionName="Sweet Dream"/>
+            <CollectionContainer image="https://cdn.donmai.us/sample/c8/f3/__large_spotted_genet_kemono_friends_and_1_more_drawn_by_aburaeoyaji__sample-c8f3de5c197618eac2b680a30fee1914.jpg" artistName="neocoill" collectionName="Sweet Dream"/>
+            <CollectionContainer image="https://cdn.donmai.us/sample/c8/f3/__large_spotted_genet_kemono_friends_and_1_more_drawn_by_aburaeoyaji__sample-c8f3de5c197618eac2b680a30fee1914.jpg" artistName="neocoill" collectionName="Sweet Dream"/>
+            <CollectionContainer image="https://cdn.donmai.us/sample/c8/f3/__large_spotted_genet_kemono_friends_and_1_more_drawn_by_aburaeoyaji__sample-c8f3de5c197618eac2b680a30fee1914.jpg" artistName="neocoill" collectionName="Sweet Dream"/>
+            <CollectionContainer image="https://cdn.donmai.us/sample/c8/f3/__large_spotted_genet_kemono_friends_and_1_more_drawn_by_aburaeoyaji__sample-c8f3de5c197618eac2b680a30fee1914.jpg" artistName="neocoill" collectionName="Sweet Dream"/>
+            <CollectionContainer image="https://cdn.donmai.us/sample/c8/f3/__large_spotted_genet_kemono_friends_and_1_more_drawn_by_aburaeoyaji__sample-c8f3de5c197618eac2b680a30fee1914.jpg" artistName="neocoill" collectionName="Sweet Dream"/>
+            <CollectionContainer image="https://cdn.donmai.us/sample/c8/f3/__large_spotted_genet_kemono_friends_and_1_more_drawn_by_aburaeoyaji__sample-c8f3de5c197618eac2b680a30fee1914.jpg" artistName="neocoill" collectionName="Sweet Dream"/>
+          </div>
         </section>
       </>
   );
